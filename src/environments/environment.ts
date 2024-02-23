9fbb7943a2159810b176768ac4c83be8981f0081
AlonSchwartz <karamon@gmail.com>
2024-02-23 12:47:17 +0200

    small bug fix

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,7 @@
 export const environment = {
     production: true,
-    serverAddress: "https://chef-curry-backend.vercel.app"
-    //serverAddress: "https://chef-curry-backend.onrender.com"
+    //serverAddress: "https://chef-curry-backend.vercel.app"
+    serverAddress: "https://chef-curry-backend.onrender.com"
 
     // private serverAddress: string = "https://chef-curry-backend.vercel.app";
     // private serverAddress: string = "https://chef-curry-backend.onrender.com"
